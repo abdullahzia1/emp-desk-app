6aae4c8f015c2dac593281b42275247860e1608c
Abdullah <abziawork960@gmail.com>
2024-12-28 05:27:04 +0500

    Initial commit

--- main.js
+++ main.js
@@ -9,31 +9,35 @@ const idleTimeout = 1 * 60 * 1000; // 5 minutes in milliseconds
 let currentState = "active"; // Tracks current state, default is "active"
 let trackingId = null; // Stores the trackingId
 let shiftStatus = false;
+let tracking = true;
 
+app.on("ready", () => {
+  startApiServer();
+});
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
+      nodeIntegration: false,
       contextIsolation: true,
-      preload: path.join(__dirname, "preload.js"),
     },
   });
 
   if (process.env.NODE_ENV === "development") {
     console.log("Dev");
-    mainWindow.loadURL("http://localhost:3000");
+    // mainWindow.loadURL("http://localhost:3000");
+    mainWindow.loadFile(path.join(__dirname, "build", "index.html"));
   } else {
     console.log("Prod");
-    mainWindow.loadURL("http://localhost:3000");
+    // mainWindow.loadURL("http://localhost:3000");
+    mainWindow.loadFile(path.join(__dirname, "build", "index.html"));
+    // mainWindow.loadFile("./build/index.html");
   }
 
   mainWindow.on("closed", () => {
     mainWindow = null;
   });
-
-  startApiServer();
 }
 
 // Function to check for mouse activity
@@ -75,6 +79,7 @@ function startMouseTracking() {
   setInterval(async () => {
     if (!trackingId) return; // Skip tracking if no trackingId
     if (shiftStatus) return;
+    if (!tracking) return;
     console.log("Starting Mouse Movements", trackingId);
     const mouseMoved = await checkMouseActivity();
 
@@ -153,6 +158,38 @@ function sendIdleRequest() {
   req.write(data);
   req.end();
 }
+function sendStopTracking() {
+  console.log("Sending stop tracking request...");
+  const data = JSON.stringify({ status: "stop", trackingId });
+
+  const options = {
+    hostname: "localhost",
+    port: 5000,
+    path: "/api/employee/status",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": data.length,
+    },
+  };
+
+  const req = http.request(options, (res) => {
+    let response = "";
+    res.on("data", (chunk) => {
+      response += chunk;
+    });
+    res.on("end", () => {
+      console.log("Idle status sent successfully:", response);
+    });
+  });
+
+  req.on("error", (error) => {
+    console.error("Error sending idle request:", error);
+  });
+
+  req.write(data);
+  req.end();
+}
 
 // Start an HTTP server to receive the trackingId
 function startApiServer() {
@@ -195,10 +232,30 @@ function startApiServer() {
         }
       });
     } else if (req.method === "POST" && req.url === "/api/logout") {
+      tracking = false;
       trackingId = null;
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Stopped Tracking" }));
+      return;
+    } else if (req.method === "POST" && req.url === "/api/end-work") {
+      tracking = false;
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Stopped Tracking" }));
+      return;
+    } else if (req.method === "POST" && req.url === "/api/start-break") {
+      tracking = false;
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Stopped Tracking" }));
+      return;
+    } else if (req.method === "POST" && req.url === "/api/end-break") {
+      tracking = true;
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Started Tracking" }));
+      return;
     } else {
       res.writeHead(404, { "Content-Type": "application/json" });
       res.end(JSON.stringify({ message: "Not Found" }));
+      return;
     }
   });
 
